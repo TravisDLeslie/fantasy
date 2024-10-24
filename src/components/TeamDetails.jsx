ee1104ebe976f4cd90b5363616ce298cfff4466f
travisdleslie <hello@travisleslie.com>
2024-10-23 18:58:44 -0600

    colors

--- src/components/TeamDetails.jsx
+++ src/components/TeamDetails.jsx
@@ -9,13 +9,13 @@ import { useDefensePoints } from '../hooks/useDefensePoints';
 import useTeamByeWeeks from '../hooks/useTeamByeWeeks';
 
 const positionStyles = {
-  QB: { text: 'text-[#FC2B6D]', bg: 'bg-[#323655]', border: 'rounded-md' },
-  RB: { text: 'text-[#20CEB8]', bg: 'bg-[#323655]', border: 'rounded-md' },
-  WR: { text: 'text-[#56C9F8]', bg: 'bg-[#323655]', border: 'rounded-md' },
-  TE: { text: 'text-[#FEAE58]', bg: 'bg-[#323655]', border: 'rounded-md' },
-  K: { text: 'text-[#C96CFF]', bg: 'bg-[#323655]', border: 'rounded-md' },
-  DEF: { text: 'text-[#BF755D]', bg: 'bg-[#323655]', border: 'rounded-md' },
-  FLEX: { text: 'text-pink-900', bg: 'bg-[#323655]', border: 'rounded-md' },
+  QB: { text: 'text-[#FC2B6D]', bg: 'bg-[#252942]', border: 'rounded-md' },
+  RB: { text: 'text-[#20CEB8]', bg: 'bg-[#252942]', border: 'rounded-md' },
+  WR: { text: 'text-[#56C9F8]', bg: 'bg-[#252942]', border: 'rounded-md' },
+  TE: { text: 'text-[#FEAE58]', bg: 'bg-[#252942]', border: 'rounded-md' },
+  K: { text: 'text-[#C96CFF]', bg: 'bg-[#252942]', border: 'rounded-md' },
+  DEF: { text: 'text-[#BF755D]', bg: 'bg-[#252942]', border: 'rounded-md' },
+  FLEX: { text: 'text-pink-900', bg: 'bg-[#252942]', border: 'rounded-md' },
 };
 
 const getPositionStyles = (position) =>
